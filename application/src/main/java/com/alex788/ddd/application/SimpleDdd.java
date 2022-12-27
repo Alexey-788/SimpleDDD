3f46e33a9371e0e4c53cc16731d483ffbc498d85
alexey.sheblykin <alexey.sheblykin1@gmail.com>
2022-12-27 20:30:08 +0300

    Starter

---
+++ application/src/main/java/com/alex788/ddd/application/SimpleDdd.java
@@ -0,0 +1,12 @@
+package com.alex788.ddd.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDdd {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDdd.class, args);
+    }
+}
