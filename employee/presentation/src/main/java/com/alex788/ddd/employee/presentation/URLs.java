e345d9a20f89efbe91221b068687da889b78b6b9
alexey.sheblykin <alexey.sheblykin1@gmail.com>
2023-01-06 11:19:52 +0300

    Rest url changed

--- employee/presentation/src/main/java/com/alex788/ddd/employee/presentation/URLs.java
+++ employee/presentation/src/main/java/com/alex788/ddd/employee/presentation/URLs.java
@@ -2,7 +2,7 @@ package com.alex788.ddd.employee.presentation;
 
 class URLs {
 
-    public static final String EMPLOYEE_ROOT = "/employee";
+    public static final String EMPLOYEE_ROOT = "/rest/v1/employee";
 
     public static final String ADD_EMPLOYEE = "/add";
     public static final String GET_ALL_EMPLOYEE = "/get-all";
